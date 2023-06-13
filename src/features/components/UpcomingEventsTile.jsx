ae2ab754795a64071054627e55ae4fc9761467a4
Kagiso Semenya <kagiso@rhest.studio>
2023-06-13 11:43:12 +0200

    initial comit

--- src/features/components/UpcomingEventsTile.jsx
+++ src/features/components/UpcomingEventsTile.jsx
@@ -12,71 +12,61 @@ import { motion } from 'framer-motion'
 import { gsap } from 'gsap';
 import {useRef, useEffect, useState} from 'react'
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import TamiaCover from '../../assets/upcomingEvents/TamiaCover.jpg'
 import { useInView } from 'react-intersection-observer'
 
 const UpcomingEventsTile = () => {
-     const imageContainer = {
-      display:"inline-block",
+  const imageContainer = {
+    display:"inline-block",
+    borderRadius: "1rem",
+    overflow: "hidden"
+  }
+
+  const singleShowContainer = {
+    width: { base:"920px", sm:"920px", md:"1440px", lg:"1440px", xl:"1440px"},
+    marginX: { base:"2rem", sm:"2rem", md:"1440px", lg:"1440px", xl:"1440px"},
+  }
+
+  const showHeadingContainer = {
+    width:"200px",
+    textAlign: "center"
+  }
+
+  const posterSize = {
+    height: { base:"400px", sm:"400px", md:"400px", lg:"600px", xl:"600px" },
+    width: { base:"400px", sm:"400px", md:"400px", lg:"600px", xl:"600px" },
+    borderRadius: "1rem",
+  }
+
+  const buttonStyle = {
+    backgroundColor:"#000", 
+    width: { base:"8rem", sm:"8rem", md:"10rem",lg:"10rem", xl:"10rem" },
+    paddingY:"1.5rem",
+    border:"1px solid #000",
+    color:"#fff", 
+    borderRadius:"5rem", 
+    fontWeight: "normal",
+    textTransform: "uppercase",
+    fontSize: { sm:"0.7rem", md:"0.85rem", lg:"0.85rem", xl:"0.85rem" },
+    fontWeight: "600",
+    
+    _hover: {
+      border:"1px solid #000",
+      backgroundColor: "#fff",
+      color: "#000",
+    }
+  }
+
+    const playlistPosterStyling = {
+      maxHeight: { base:"310px", sm:"310px", md:"450px", lg:"450px", xl:"450px" },
+      maxWidth: { base:"310px", sm:"310px", md:"450px", lg:"450px", xl:"450px" },
       borderRadius: "1rem",
-      overflow: "hidden"
-     }
+    }
     
-      const topLeftBorder = {
-        height:"70px",
-        width:"70px",
-        borderLeft:"1px solid #000",
-      
-        borderTop:"1px solid #000",
-      }
-    
-      const bottomRightBorder = {
-        marginLeft:{lg:"80%", xl:"87%"},
-        marginTop:"-4%",
-        display: { base:"none", sm:"none", md:"none", lg:"block", xl:"block"},
-        position:"relative",
-        height:"70px",
-        width:"70px",
-        borderRight:"1px solid #000",
-        borderBottom:"1px solid #000",
-      }
-
-      const posterSize = {
-        height: "550px",
-        width: "550px",
-        borderRadius: "1rem",
-      }
-
-      const buttonStyle = {
-        backgroundColor:"#000", 
-        width: "10rem",
-        paddingY:"1.5rem",
-        border:"1px solid #000",
-        color:"#fff", 
-        borderRadius:"5rem", 
-        fontWeight: "normal",
-        textTransform: "uppercase",
-        fontSize: "0.85rem",
-        fontWeight: "600",
-        
-        _hover: {
-          border:"1px solid #000",
-          backgroundColor: "#fff",
-          color: "#000",
-        }
-      }
-
-      const playlistPosterStyling = {
-        height:"400px",
-        width:"400px",
-        borderRadius: "1rem",
-      }
-      
-      const playlistPosterButtonContainer = {
-        display:"flex", 
-        justifyContent:"space-between", 
-        margin:"1rem 0"
-      }
+    const playlistPosterButtonContainer = {
+      display:"flex", 
+      justifyContent: "space-between", 
+      margin:"1rem 0"
+    }
 
       //  Framer Motion
       const MotionFlex = motion(Flex);
@@ -84,15 +74,6 @@ const UpcomingEventsTile = () => {
       const MotionHeading = motion(Heading);
       const MotionBox = motion(Box);
       const { ref, inView } = useInView();
-
-      const containerVariants = {
-        hover: {
-          scale: 1.03,
-          transition: {
-            duration: 0.7,
-          }
-        }
-      }
 
       const posterVariants = {
         hidden: {
@@ -110,30 +91,6 @@ const UpcomingEventsTile = () => {
             ease: 'easeInOut', // Choose the desired easing function
           },
         },
-      }
-
-      const showNameVariants = {
-        hidden: {
-          opacity: 0,
-          x: -30
-        },
-        visible: {
-          opacity: 1,
-          x: 0,
-          transition: {delay: 1.2, duration: 1.2}
-        }
-      }
-
-      const showDateVariants = {
-        hidden: {
-          opacity: 0,
-          x: 30
-        },
-        visible: {
-          opacity: 1,
-          x: 0,
-          transition: {delay: 1.2, duration: 1.2}
-        }
       }
 
       const headingVariants = {
@@ -184,15 +141,16 @@ const UpcomingEventsTile = () => {
             };
       
             if (screenWidth < breakpoints.tiny) {
-              return '-1270vw';
+              return '-3440px';
             } else if (screenWidth < breakpoints.small) {
-              return '-920vw';
+              return '-3440px';
+              // 1440 * 3 + 32
             } else if (screenWidth < breakpoints.medium) {
-              return '-615vw';
+              return '-4336px';
             } else if (screenWidth < breakpoints.large) {
-              return '-445vw';
+              return '-4336px'; 
             } else {
-              return '-300vw';
+              return '-4336px';
             }
           }
 
@@ -204,7 +162,11 @@ const UpcomingEventsTile = () => {
       
      
   return (
-    <Box paddingX={{ base:"0.25rem", md:"0.25rem", lg:"2rem", xl:"2rem"}} marginY="6rem" display= {{ base:"none", sm:"none",   md:"none", lg:"block", xl:"block" }}>
+    <Box 
+      // paddingX= {{ base:"0.25rem", md:"0.25rem", lg:"2rem", xl:"2rem"}}  
+      marginY="6rem" 
+      // display= {{ base:"none", sm:"none",   md:"none", lg:"block", xl:"block" }}
+    >
         <MotionHeading 
           variant="h3" 
           as="h3" 
@@ -226,21 +188,22 @@ const UpcomingEventsTile = () => {
             <Box
               className="scroll-section-inner"
               height="100vh"
-              width="400wv"
+              width="5760px"
               display="flex"
               // justifyContent="center"
               alignItems="center"
               position="relative"
               ref={sectionRef}
             >
-              <Box display="flex" width="300vw">
-                <Box width="100vw">
+              <Box display="flex" justifyContent="center">
+                <Box sx={singleShowContainer} >
                   <Flex
                     // justifyContent="center"
                     alignItems="center"
-                    width="100vw"
+                    justifyContent="center"
+                   
                   >
-                    <Heading variant="h3" width="15%">
+                    <Heading variant="h3" sx={showHeadingContainer}>
                       Inkabi <br/>Zezwe
                     </Heading>
                       <MotionImage
@@ -285,152 +248,153 @@ const UpcomingEventsTile = () => {
                     </Flex>
                   </Flex>
                 </Box>
-                <Box width="100vw">
+                <Box sx={singleShowContainer}>
                   <Flex
                     // justifyContent="center"
                     alignItems="center"
-                    width="100vw"
+                    justifyContent="center"
+                   
                   >
-                    <Heading variant="h3" width="20%">
-                      Tamia Live in South Africa
+                    <Heading variant="h3" sx={showHeadingContainer}>
+                      Tamia Live In South Africa
                     </Heading>
-                    <MotionImage
-                      src={TamiaSquare}
-                      variants={posterVariants}
-                      initial="hidden"
-                      animate="visible"
-                      sx={posterSize}
-                      marginX="2rem"
-                      position="relative"
-                    />
-                    <Box>
-                      <Box sx={imageContainer}>
-                        <MotionImage
-                          src={TamiaPlaylist}
-                          variants={posterVariants}
-                          initial="hidden"
-                          animate="visible"
-                          whileHover="hover"
-                          sx={playlistPosterStyling}
-                        />
+                      <MotionImage
+                        src={TamiaSquare}
+                        variants={posterVariants}
+                        initial="hidden"
+                        animate="visible"
+                        marginX="2rem"
+                        sx={posterSize}
+                        
+                        position="relative"
+                      />
+                    <Flex height="100%" flexDir="column">
+                      <Box>
+                        <Box sx={imageContainer}>
+                          <MotionImage
+                            src={TamiaPlaylist}
+                            variants={posterVariants}
+                            initial="hidden"
+                            animate="visible"
+                            whileHover="hover"
+                            sx={playlistPosterStyling}
+                          />
+                        </Box>
+                        <Box sx={playlistPosterButtonContainer}>
+                          <Link isExternal href="https://www.ticketpros.co.za/portal/web/index.php/parent_event/1915f22b-8cf5-c5c3-9449-642286c5cad5">
+                            <Button
+                              sx={buttonStyle}
+                            >
+                              Purchase Tickets
+                            </Button>
+                          </Link>
+                          <Link isExternal href="https://music.apple.com/za/playlist/tamia-essentials/pl.1c127f3670d34209a8efa4143d2a427b">
+                            <Button 
+                              sx={buttonStyle}
+                            >
+                              Playlist
+                            </Button>
+                          </Link>
+                        </Box>
                       </Box>
-                      <Box sx={playlistPosterButtonContainer} >
-                        <Link isExternal href="https://www.ticketpros.co.za/portal/web/index.php/parent_event/1915f22b-8cf5-c5c3-9449-642286c5cad5">
-                          <Button 
-                            sx={buttonStyle}
-                          >
-                            Purchase Tickets
-                          </Button>
-                        </Link>
-                        <Link 
-                          isExternal 
-                          href="https://music.apple.com/za/playlist/tamia-essentials/pl.1c127f3670d34209a8efa4143d2a427b"
-                        >
-                          <Button 
-                            sx={buttonStyle}
-                          >
-                            Playlist
-                          </Button>
-                        </Link>
-                      </Box>
-                    </Box>
+                    </Flex>
                   </Flex>
                 </Box>
-                <Box width="100vw">
+                <Box sx={singleShowContainer}>
                   <Flex
                     // justifyContent="center"
                     alignItems="center"
-                    width="100vw"
+                    justifyContent="center"
+                   
                   >
-                    <Heading variant="h3" width="20%">
-                      Boyz II Men
+                    <Heading variant="h3" sx={showHeadingContainer}>
+                      Boyz II Men South African Tour
                     </Heading>
-                    <MotionImage
-                      src={BoysIIMenSquare}
-                      variants={posterVariants}
-                      initial="hidden"
-                      animate="visible"
-                      sx={posterSize}
-                      marginX="2rem"
-                      position="relative"
-                    />
-                    <Box>
-                      <Box sx={imageContainer}>  
-                        <MotionImage
-                          src={BoyzIIMenPlaylist}
-                          variants={posterVariants}
-                          initial="hidden"
-                          animate="visible"
-                          whileHover="hover"
-                          sx={playlistPosterStyling}
-                        />
+                      <MotionImage
+                        src={BoysIIMenSquare}
+                        variants={posterVariants}
+                        initial="hidden"
+                        animate="visible"
+                        marginX="2rem"
+                        sx={posterSize}
+                        
+                        position="relative"
+                      />
+                    <Flex height="100%" flexDir="column">
+                      <Box>
+                        <Box sx={imageContainer}>
+                          <MotionImage
+                            src={BoyzIIMenPlaylist}
+                            variants={posterVariants}
+                            initial="hidden"
+                            animate="visible"
+                            whileHover="hover"
+                            sx={playlistPosterStyling}
+                          />
+                        </Box>
+                        <Box sx={playlistPosterButtonContainer}>
+                          <Link isExternal href="https://www.ticketpros.co.za/portal/web/index.php/parent_event/810aa03b-d57e-d4eb-10a4-63fe581c39ac?sort_order=A-Z&category=music&province=&from=&to=">
+                            <Button
+                              sx={buttonStyle}
+                            >
+                              Purchase Tickets
+                            </Button>
+                          </Link>
+                          <Link isExternal href="https://music.apple.com/za/playlist/boyz-ii-men-essentials/pl.95bfdb84270a41509e118bb25955f0b0">
+                            <Button 
+                              sx={buttonStyle}
+                            >
+                              Playlist
+                            </Button>
+                          </Link>
+                        </Box>
                       </Box>
-                    <Box sx={playlistPosterButtonContainer}>
-                      <Link 
-                        isExternal 
-                        href="https://www.ticketpros.co.za/portal/web/index.php/parent_event/810aa03b-d57e-d4eb-10a4-63fe581c39ac?sort_order=A-Z&category=music&province=&from=&to="
-                      >
-                        <Button 
-                          sx={buttonStyle}
-                        >
-                          Purchase Tickets
-                        </Button>
-                      </Link>
-                      <Link isExternal href="https://music.apple.com/za/playlist/boyz-ii-men-essentials/pl.95bfdb84270a41509e118bb25955f0b0">
-                        <Button 
-                          sx={buttonStyle}
-                        >
-                          Playlist
-                        </Button>
-                      </Link>
-                    </Box>
-                    </Box>
+                    </Flex>
                   </Flex>
                 </Box>
-                <Box width="100vw">
+                <Box sx={singleShowContainer}>
                   <Flex
                     // justifyContent="center"
                     alignItems="center"
-                    width="100vw"
+                    justifyContent="center"
+                   
                   >
-                    <Heading variant="h3" width="20%">
+                    <Heading variant="h3" sx={showHeadingContainer}>
                       Hey Neighbour
                     </Heading>
-                    <MotionImage
-                      src={HeyNeighbourSquare}
-                      variants={posterVariants}
-                      initial="hidden"
-                      animate="visible"
-                      sx={posterSize}
-                      marginX="2rem"
-                      position="relative"
-                    />
-                    <Box>
-                      <Box sx={imageContainer}>
-                        <MotionImage
-                          src={HeyNeighbourPlaylist}
-                          variants={posterVariants}
-                          initial="hidden"
-                          animate="visible"
-                          whileHover="hover"
-                          sx={playlistPosterStyling}
-                        />
+                      <MotionImage
+                        src={HeyNeighbourSquare}
+                        variants={posterVariants}
+                        initial="hidden"
+                        animate="visible"
+                        marginX="2rem"
+                        sx={posterSize}
+                        
+                        position="relative"
+                      />
+                    <Flex height="100%" flexDir="column">
+                      <Box>
+                        <Box sx={imageContainer}>
+                          <MotionImage
+                            src={HeyNeighbourPlaylist}
+                            variants={posterVariants}
+                            initial="hidden"
+                            animate="visible"
+                            whileHover="hover"
+                            sx={playlistPosterStyling}
+                          />
+                        </Box>
+                        <Box sx={playlistPosterButtonContainer}>
+                          <Link isExternal href="https://www.ticketpros.co.za/portal/web/index.php?page_id=event&event_id=5077564a-6bcc-a263-fb88-642d3e6c4844">
+                            <Button
+                              sx={buttonStyle}
+                            >
+                              Purchase Tickets
+                            </Button>
+                          </Link>
+                        </Box>
                       </Box>
-                    <Box sx={playlistPosterButtonContainer}>
-                      <Link isExternal href="https://www.ticketpros.co.za/portal/web/index.php?page_id=event&event_id=5077564a-6bcc-a263-fb88-642d3e6c4844">
-                        <Button 
-                          sx={buttonStyle}
-                        >
-                          Purchase Tickets
-                        </Button>
-                      </Link>
-                      {/* <Button 
-                        sx={buttonStyle}
-                      >
-                        Playlist
-                      </Button> */}
-                    </Box>
-                    </Box>
+                    </Flex>
                   </Flex>
                 </Box>
               </Box>
