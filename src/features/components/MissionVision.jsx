488b703e011b9d5d3d4d0c126da83a8737ab80d3
Kagiso Semenya <kagiso@rhest.studio>
2023-06-27 07:55:13 +0200

    hey neighbour

--- src/features/components/MissionVision.jsx
+++ src/features/components/MissionVision.jsx
@@ -69,10 +69,6 @@ const MissionVission = () => {
         paddingBottom:"1rem"
     }
 
-    const twoColTextStyling = {
-        margin:"2rem"
-    }
-
 
     // Framer Motion 
     const MotionGridItem = motion(GridItem);
